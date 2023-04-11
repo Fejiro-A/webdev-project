943f63276dd4dc7f8c0a6a3ecc046ce74c65a7cf
cxbns <megan.brandreth@ontariotechu.net>
2023-04-11 17:38:18 -0400

    Code Formatting
    Using the Prettier code formatter, maintaining industry standard practice

--- web_api/mongo-client.js
+++ web_api/mongo-client.js
@@ -1,25 +1,25 @@
 /**
  * This file defines a uniform way to get a MongoClient instance connected to a mongodb process
+ * @prettier
  */
-const mongoClient = require('mongodb').MongoClient;
-const projectConstants = require('./constants');
 
-console.log(`Mongo client: ${projectConstants.MONGODB_URL}`);
+const mongoClient = require('mongodb').MongoClient
+const projectConstants = require('./constants')
+
+console.log(`Mongo client: ${projectConstants.MONGODB_URL}`)
 
 async function generateClient() {
-    let client = null;
-    try {
-        client = await mongoClient.connect(projectConstants.MONGODB_URL);
-    } // end try
-    catch (e) {
-        console.log(e);
-    } // end catch
+  let client = null
+  try {
+    client = await mongoClient.connect(projectConstants.MONGODB_URL)
+  } catch (e) {
+    // end try
+    console.log(e)
+  } // end catch
 
-    console.log("Connected to mongo db");
+  console.log('Connected to mongo db')
 
-    return client;
+  return client
 } // end client generation
 
-
-
-module.exports = generateClient;
+module.exports = generateClient
--- web_api/passport-jwt-config.js
+++ web_api/passport-jwt-config.js
@@ -1,38 +1,42 @@
-const { MongoClient, ObjectId } = require('mongodb');
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const constants = require('./constants');
-
+const { MongoClient, ObjectId } = require('mongodb')
+const passport = require('passport')
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const constants = require('./constants')
 
 var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = constants.JWT_SECRET;
-opts.issuer = constants.JWT_ISSUER;
-opts.audience = constants.JWT_AUDIENCE;
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = constants.JWT_SECRET
+opts.issuer = constants.JWT_ISSUER
+opts.audience = constants.JWT_AUDIENCE
 
 /**
  * Configure passport jwt strategy
- * @param {MongoClient} client 
+ * @param {MongoClient} client
+ * @prettier
  */
 
 async function configure(client) {
-    let User = await client.db().collection(constants.MONGO_USER_COLLECTION_NAME);
-    passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-        // Look for a user with the id in the jwt payload
-        User.find({ _id: new ObjectId(jwt_payload.sub) })
-            .limit(1).next()
-            .then((user) => {
-                // Block request if user could not be found
-                if (!user) return done(null, false);
+  let User = await client.db().collection(constants.MONGO_USER_COLLECTION_NAME)
+  passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+      // Look for a user with the id in the jwt payload
+      User.find({ _id: new ObjectId(jwt_payload.sub) })
+        .limit(1)
+        .next()
+        .then(user => {
+          // Block request if user could not be found
+          if (!user) return done(null, false)
 
-                // Authorize
-                return done(null, user);
-            }).catch((err) => {
-                // Some unexpected error
-                if (err) return done(err);
-            }) // end catch
-    })); // end passport jwt config
+          // Authorize
+          return done(null, user)
+        })
+        .catch(err => {
+          // Some unexpected error
+          if (err) return done(err)
+        }) // end catch
+    })
+  ) // end passport jwt config
 } // end configuration
 
-module.exports = configure;
+module.exports = configure
