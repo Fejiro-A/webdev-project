7a9f192a033b4af356693fe360de8fc225cf2789
Cameron-Millar <cameron.millar@ontariotechu.net>
2023-04-18 19:13:15 -0400

    added enter key input for signup and login

--- scripts/signup.js
+++ scripts/signup.js
@@ -36,4 +36,44 @@ $(document).ready(function() {
             });
         }
     });
+    //submit info when enter key is pressed
+    $("#firstname, #lastname, #username, #password").on("keyup", function(e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            var username = $("#username").val();
+            var password = $("#password").val();
+            var firstName = $("#firstname").val();
+            var lastName = $("#lastname").val();
+
+            if (username && password && firstName && lastName) {
+                $.ajax({
+                    type: "POST",
+                    url: "http://localhost:3000/auth/register",
+                    data: JSON.stringify({ username: username, password: password, firstName: firstName, lastName: lastName }),
+                    contentType: "application/json",
+                    success: function(response) {
+                        $.ajax({
+                            type: "POST",
+                            url: "http://localhost:3000/auth/login",
+                            data: JSON.stringify({ username: username, password: password }),
+                            contentType: "application/json",
+                            success: function(response) {
+                                // Store the JWT token in the localStorage
+                                localStorage.setItem("token", response.token);
+
+                                // Redirect to the main chat page
+                                window.location.href = "../pages/users.html";
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                alert("Failed to login. Please try again.");
+                                window.location.href = "../pages/login.html";
+                            }
+                        });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        alert("Registration failed. Please try again.");
+                    }
+                });
+            }
+        }
+    });
 });
